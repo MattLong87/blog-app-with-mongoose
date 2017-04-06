e04069ef88a0ca14762415eba5ed0459ded95cb3
Matthew Long <MattLong87@gmail.com>
2017-04-06 13:04:10 -0400

    Implemented put and delete routes, added validation to post

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -9,7 +9,7 @@ const jsonParser = bodyParser.json();
 
 const {Post} = require('./models');
 
-router.use(validator);
+//router.use(validator);
 
 //GET /posts sends back all posts in the database
 router.get('/', (req, res) => {
@@ -18,7 +18,8 @@ router.get('/', (req, res) => {
 		.then(posts => {
 			res.json({
 				posts: posts.map(
-					(post) => post.formatPost())
+					(post) => post.formatPost()
+					)
 			});
 		})
 		.catch(
@@ -46,6 +47,23 @@ router.get('/:id', (req, res) => {
 
 //POST /posts endpoint for creating new blog posts
 router.post('/', jsonParser, (req, res) => {
+	//validating all required fields are present and sending a 400 error if any missing
+	//should probably also validate author firstname and lastname
+	const requiredFields = ["title", "content", "author"];
+	let missingFields = [];
+	for (var i = 0; i < requiredFields.length; i++){
+		if (!(requiredFields[i] in req.body)){
+			missingFields.push(requiredFields[i]);
+		}
+	}
+	if(missingFields.length > 0){
+		errorMessage = "";
+		missingFields.forEach(field => {
+			errorMessage += field + " is required. ";
+		})
+		res.status(400).json({error: errorMessage});
+	}
+	//if all fields were present, create post
 	Post.create({
 		title: req.body.title,
 		content: req.body.content,
@@ -59,6 +77,38 @@ router.post('/', jsonParser, (req, res) => {
 });
 
 //PUT /posts/:id endpoint that allows you to update the title, content, and author fields.
+router.put('/:id', jsonParser, (req, res) => {
+	if (!(req.body.id && req.body.id === req.params.id)){
+		res.status(400).json({message: "ID in body must match URL path ID"})
+	}
+	const toUpdate = {};
+	for (var field in req.body){
+		if (field != "id"){
+			toUpdate[field] = req.body[field];
+			//below doesn't work:
+			// Post.findByIdAndUpdate(req.params.id, {$set: {field: req.body[field]}}, {new: true})
+			// .exec()
+			// .then(post => {
+			// 	console.log(post);
+			// 	res.json(post);
+			// })
+		}
+	}
+	Post.findByIdAndUpdate(req.params.id, {$set: toUpdate}, {new: true})
+	.exec()
+	.then(post => {
+		res.json(post.formatPost());
+	})
+	.catch(err => res.status(500).json({message: 'Internal server error'}))
+});
 
 //DELETE /posts/:id allows you to delete a post with a given id.
+router.delete('/:id', (req, res) => {
+	console.log(req.params.id);
+	Post.findByIdAndRemove(req.params.id)
+	.exec()
+	.then(() => res.status(204).end())
+	.catch(err => res.status(500).json({message: 'Internal server error'}))
+});
+
 module.exports = router;
